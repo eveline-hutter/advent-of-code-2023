8125c4ef8d99aaa96b3f893cf589a6b56abd497f
eveline-hutter <57252586+eveline-hutter@users.noreply.github.com>
2023-12-01 10:28:42 +0100

    solved day1, puzzle1

--- day1/main1.py
+++ day1/main1.py
@@ -1,4 +1,11 @@
 with open('input1.txt') as file:
     d1_input = file.read().splitlines()   # open without /n
 
-print(d1_input)
+
+def puzzle1():
+    calibration_values = [[value for value in line if value.isnumeric()] for line in d1_input]
+    sum_of_calibration_values = sum(int(digits[0] + digits[-1]) for digits in calibration_values)
+    print('sum of all calibration values:', sum_of_calibration_values)
+
+
+puzzle1()
